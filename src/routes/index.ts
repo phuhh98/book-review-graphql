770d5e61a292fb466390bd10d505fcc207ca554b
phuhh98 <phuhh98@gmail.com>
2023-08-03 20:42:15 +0700

    refactor: getApolloServer => getApolloMiddleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,6 @@
-import { expressMiddleware } from '@apollo/server/express4';
 import { Application } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { getApolloMiddleware, getApolloServer } from './graphql';
+import { getApolloMiddleware } from './graphql';
 
 export async function applyRoutes(app: Application) {
   const apolloMidleware = await getApolloMiddleware(app);
