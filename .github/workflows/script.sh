782de4cad3550fb344d1a525245e5e60f8aa3e05
phuhh98 <phuhh98@gmail.com>
2023-09-26 22:15:14 +0700

    update script

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -2,7 +2,7 @@ echo "BRANCH_NAME" $BRANCH_NAME
 echo "SERVER_NAME" $SERVER_NAME
 echo "APP_UNDER_MAINTAINANCE" $APP_UNDER_MAINTAINANCE
 
-cd ~/book-review-graphql-BE/
+cd ~/projects/book-review-graphql-BE/
 
 git checkout $BRANCH_NAME
 git reset --hard HEAD
